6dc73aa044a77cc7ae9f7518cd58a3088b94d6d4
Alex <alexgabriel212016@gmail.com>
2025-11-03 12:15:30 -0300

    atualizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Cabecalho from "./componentes/Cabecalho";
+import Cabecalho from "./componentes/CCabecalho";
 import Inicio from "./componentes/Inicio";
 import Criar from "./componentes/Criar";
 import Ler from "./componentes/Ler";
 import Alterar from "./componentes/Alterar";
-import Apagar from "./componentes/Apagar";
+import Apagar from "./componentes/Excluir";
 
 export default function App() {
   return (
